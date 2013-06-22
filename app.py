e497c09ea0db62ec3c3896d5e3f8ad8d1f3b810b
Joaquín Sorianello <joac@gcoop.coop>
2013-06-22 12:55:48 -0300

    Added main window, and basic screen handling

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from PyQt4 import QtGui, QtCore
 
 class CalibratorApp(QtGui.QWidget):
 
-    def __init__(self):
+    def __init__(self, model):
         super(CalibratorApp, self).__init__()
         self.position = QtCore.QPoint(0 , 0)
-        self.setWindowTitle('Calibrator')
+        self.setWindowTitle('Calibrator - %s' % model)
         self.setMouseTracking(True)
         self.points = []
         self.painter = QtGui.QPainter()
@@ -51,6 +51,9 @@ class CalibratorApp(QtGui.QWidget):
         qp.drawLine(x - 5, y - 5, x + 5, y + 5)
         qp.drawLine(x - 5, y + 5, x + 5, y - 5)
         qp.drawText(QtCore.QPoint(x, y), "(%d, %d)" % (x, y ))
+        #self.emit(QtCore.SIGNAL('my_signal()'), )
+
+        #Emito señal
 
     def mouseMoveEvent(self, event):
         self.position = event.pos()
@@ -96,10 +99,97 @@ class CalibratorApp(QtGui.QWidget):
         print cv2.calibrateCamera()
 
 
+class PanelWidget(QtGui.QWidget):
+
+    def __init__(self):
+        super(PanelWidget, self).__init__()
+        self.buildUI()
+
+    def buildUI(self):
+        v_layout = QtGui.QVBoxLayout()
+
+        self.desktop_widget = QtGui.QDesktopWidget()
+        self.desktop_widget.screenCountChanged.connect(self.obtain_screens)
+        # Main window shows:
+        # List of screens, to choose calibration target
+        # Button "Calibrate intrinsec parameters"
+        # List of calibrated devices
+        # Button "Calculate projector position"
+
+        self.screen_list = QtGui.QComboBox()
+        self.obtain_screens()
+
+        self.calibrate_button = QtGui.QPushButton('Calibrate Projector')
+        self.calibrated_list = QtGui.QComboBox()
+        self.calculate_button = QtGui.QPushButton('Calculate Position')
+        v_layout.addWidget(self.screen_list)
+        v_layout.addWidget(self.calibrate_button)
+        v_layout.addWidget(self.calibrated_list)
+        v_layout.addWidget(self.calculate_button)
+
+        self.calibrate_button.clicked.connect(self.calibrate)
+        self.setLayout(v_layout)
+        self.show()
+
+    def calibrate(self):
+        target_screen =  self.screen_list.currentIndex()
+        target_name =  self.screen_list.itemText(target_screen)
+        print target_screen, target_name
+        text, ok = QtGui.QInputDialog.getText(self, 'Projector Name', 'Enter projector model:')
+        if ok:
+            #FIXME Save calibration data of Projector
+
+            self.calibrator = CalibratorApp(text)
+            # Conectar señal a ventana de actualizacion
+           # self.calibrator.signal.connect(self.view)
+
+    def obtain_screens(self):
+        self.screen_list.clear()
+        for i in xrange(0, self.desktop_widget.screenCount()):
+            geometry = self.desktop_widget.screenGeometry(i)
+            label = u'Screen %d - %dx%d' % (i, geometry.width(), geometry.height())
+            self.screen_list.addItem(label)
+
+
+
+
+class MainWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.buildUI()
+
+    def buildUI(self):
+        exit_action = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self)
+        exit_action.setShortcut('Ctrl+Q')
+        exit_action.setStatusTip('Exit Aplication')
+        exit_action.triggered.connect(QtGui.qApp.quit)
+
+        load_projector_action =  QtGui.QAction(QtGui.QIcon('open.png'), '&Load Projector', self)
+
+        load_projector_action.setShortcut('Ctrl+L')
+        load_projector_action.setStatusTip('Load intrinsec calibration data for projector')
+        load_projector_action.triggered.connect(self.load_projector)
+
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('&File')
+        file_menu.addAction(load_projector_action)
+        file_menu.addAction(exit_action)
+        self.panel = PanelWidget()
+        self.setCentralWidget(self.panel)
+
+        self.setWindowTitle('Fugue - Calibration')
+        self.statusBar().showMessage('Ready')
+        self.show()
+
+    def load_projector(self):
+        print "It works"
+
+
 def main():
 
     app = QtGui.QApplication(sys.argv)
-    widget = CalibratorApp()
+    main = MainWindow()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
